2ca0148e4c0dcf826ef9e201ada10e07a3ff7268
Harshit Jaysan Sharma <113779751+harshu555-4999@users.noreply.github.com>
2026-06-13 17:13:07 +0530

    Change USAJOBS API key and email
    Updated USAJOBS_API_KEY and USAJOBS_EMAIL with new values.

--- utils/config.py
+++ utils/config.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-USAJOBS_API_KEY = os.getenv("USAJOBS_API_KEY")
-USAJOBS_EMAIL = os.getenv("USAJOBS_EMAIL")
+USAJOBS_API_KEY = os.getenv("3AtGRZS3Y6nZ6PSosfzm1bbNIDF1tfp/mqziXMS8+ro=")
+USAJOBS_EMAIL = os.getenv("1008hjs@gmail.com")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
