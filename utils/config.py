2a437fbf702ed710693475cfa7a849a46854ca84
Harshit Jaysan Sharma <113779751+harshu555-4999@users.noreply.github.com>
2026-06-13 17:16:26 +0530

    Hardcode USAJOBS API key and email in config
    Replaced environment variable loading for USAJOBS API key and email with hardcoded values.

--- utils/config.py
+++ utils/config.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-USAJOBS_API_KEY = os.getenv("3AtGRZS3Y6nZ6PSosfzm1bbNIDF1tfp/mqziXMS8+ro=")
-USAJOBS_EMAIL = os.getenv("1008hjs@gmail.com")
+#USAJOBS_API_KEY = os.getenv("3AtGRZS3Y6nZ6PSosfzm1bbNIDF1tfp/mqziXMS8+ro=")
+#USAJOBS_EMAIL = os.getenv("1008hjs@gmail.com")
+USAJOBS_API_KEY = "3AtGRZS3Y6nZ6PSosfzm1bbNIDF1tfp/mqziXMS8+ro="
+USAJOBS_EMAIL = "1008hjs@gmail.com"
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
